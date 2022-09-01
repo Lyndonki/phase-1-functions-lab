6b4ba7ac102c2672be8a1c5ef9aee3141c446b09
Xanderk <Xanderkidwell@gmail.com>
2022-09-01 15:23:40 -0700

    commit tree 77f4315a8310167ced5310b82fe9e8a5f54632fa

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) { return blocks - 42 } else { return 42 - blocks }
+}
+
+
+function distanceFromHqInFeet(feetHq) {
+    let distanceBlocks = distanceFromHqInBlocks(feetHq);
+    if (distanceBlocks > 42) { return distanceBlocks * 264 } else { return distanceBlocks * 264 }
+}
+
+const distanceTravelledInFeet = function (feet, feet2) {
+    if (feet, feet2 > 42) { return (feet2 - feet) * 264 } else { return (feet - feet2) * 264 }
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0
+    } else if (distance < 1600) {
+        return 0.02 * (distance - 400)
+    } else if (2500 > distance) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
